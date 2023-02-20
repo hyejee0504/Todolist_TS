daa4de50392839470c6dd3ac5305a8716ae6baeb
hyejihan <108308915+hyejee0504@users.noreply.github.com>
2023-02-20 21:39:59 +0900

    feat: 할일 삭제하는 기능 구현, 체크박스추가, input창 초기화

--- my-app/src/components/TodoInput.tsx
+++ my-app/src/components/TodoInput.tsx
@@ -1,20 +1,25 @@
-import {useState} from 'react';
+import {useRef, useState} from 'react';
 export  interface Iprops {
     changeTodovalue: (x : string) => void
   }
 
 export default function TodoInput(props: Iprops) {
     const [todoinput, setTodoinput] = useState("");
+    const ref = useRef<HTMLInputElement>(null);
 
     const handletodolist = () => {
-        props.changeTodovalue(todoinput)
+        props.changeTodovalue(todoinput);
+        if(ref.current){
+          ref.current.value = "";
+        }
     }
     const handlechangeinput = (e: React.ChangeEvent<HTMLInputElement>) => {
         setTodoinput(e.target.value);
+        // e.target.value = 
     }
   return (
     <>
-        <input type="text" onChange={handlechangeinput} />
+        <input type="text" ref={ref} onChange={handlechangeinput} />
         <button onClick={handletodolist}>입력</button>
     </>
   )
--- my-app/src/components/Todolist.tsx
+++ my-app/src/components/Todolist.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 /* eslint-disable array-callback-return */
 import React, { useState, useEffect } from 'react'
 export  interface Iprops {
@@ -6,6 +7,16 @@ export  interface Iprops {
 
 export default function Todolist(props: Iprops) {
     const [todoArr, setTodoArr] = useState<string[]>([]);
+    let arr: string[] = [];
+
+    const handledelete = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();    
+        todoArr.map(value => {
+            if(value !== e.currentTarget.parentElement?.innerText.slice(0, -1)){
+            arr.push(value);
+        }})
+        setTodoArr(arr)        
+    }
 
     useEffect(() => {
         setTodoArr(todoArr => [...todoArr, props.todovalues]);
@@ -16,7 +27,9 @@ export default function Todolist(props: Iprops) {
         {todoArr.length > 0 ?
         todoArr.map((value, index) => {
             if(value !== ""){
-                return <li key={index+1}>{value}</li>
+                return <li key={index+1}><input type="checkbox" />{value}
+                <button onClick={handledelete}>x</button>
+                </li>
             }
         }) : null}
     </ul>
